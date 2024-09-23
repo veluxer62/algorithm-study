ba7e9848d9afb132cdde93441a5dbca9463741ab
kei <veluxer62@gmail.com>
2024-09-23 21:17:51 +0900

    feat: 4. 가장 흔한 단어

---
+++ src/test/kotlin/com/example/algorithmstudy/KotlinMostCommonWordTest.kt
@@ -0,0 +1,36 @@
+package com.example.algorithmstudy
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class KotlinMostCommonWordTest {
+    /*
+     * 금지된 단어를 제외하고 가장 흔하게 등장하는 단어를 출력하라. 대소문자를 구분하지 않으며, 구두점(마침표, 쉼표 등) 또한 무시한다.
+     */
+
+    @Test
+    fun test_mostCommonWord() {
+        val paragraph = "Ross hit a ball, the hit BALL flew far away after it was hit."
+        val banned = arrayOf("hit")
+        val actual =
+            record {
+                mostCommonWord(paragraph, banned)
+            }
+        Assertions.assertEquals("ball", actual)
+    }
+
+    private fun mostCommonWord(
+        paragraph: String,
+        banned: Array<String>,
+    ): String {
+        val words = paragraph.replace("\\W+".toRegex(), " ").lowercase().split(" ")
+        val counts = mutableMapOf<String, Int>()
+        for (word in words) {
+            if (!banned.contains(word)) {
+                counts[word] = counts.getOrDefault(word, 0) + 1
+            }
+        }
+
+        return counts.maxBy { it.value }.key
+    }
+}
