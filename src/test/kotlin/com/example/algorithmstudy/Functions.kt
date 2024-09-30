eb4c5d7298be869fbf71178dfc8a615a3bea9d35
kei <veluxer62@gmail.com>
2024-09-30 23:43:37 +0900

    feat: 도움 함수 추가

--- src/test/kotlin/com/example/algorithmstudy/Functions.kt
+++ src/test/kotlin/com/example/algorithmstudy/Functions.kt
@@ -1,5 +1,26 @@
 package com.example.algorithmstudy
 
+import kotlin.random.Random
+
 fun <T> record(fn: RunningTimeRecorder<T>): T {
     return fn.record()
+}
+
+fun <T> assertContains(
+    expected: Collection<T>,
+    actual: Collection<T>,
+) {
+    if (!expected.containsAll(actual)) {
+        throw AssertionError("$expected are not contains $actual")
+    }
+}
+
+fun generateIntArray(size: Int): IntArray {
+    val randomArray = IntArray(size)
+
+    for (i in 0 until size) {
+        randomArray[i] = Random.nextInt()
+    }
+
+    return randomArray
 }
