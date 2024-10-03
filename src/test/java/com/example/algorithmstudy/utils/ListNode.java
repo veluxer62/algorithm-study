d8e9a0939d680b2d44a31b7cd0193b80ca4e50c1
kei <veluxer62@gmail.com>
2024-10-03 21:31:00 +0900

    feat: ListNode에 equals, hashCode, toString 구현

--- src/test/java/com/example/algorithmstudy/utils/ListNode.java
+++ src/test/java/com/example/algorithmstudy/utils/ListNode.java
@@ -1,5 +1,6 @@
 package com.example.algorithmstudy.utils;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListNode {
@@ -17,5 +18,37 @@ public class ListNode {
             temp.next = new ListNode(list.get(i));
             temp = temp.next;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        var me = toLinkedList(this);
+        var other = toLinkedList((ListNode)o);
+        return me.equals(other);
+    }
+
+    private LinkedList<Integer> toLinkedList(ListNode node) {
+        var result = new LinkedList<Integer>();
+        var temp = node;
+
+        while (temp != null) {
+            result.add(temp.val);
+            temp = temp.next;
+        }
+
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return toLinkedList(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return toLinkedList(this).toString();
     }
 }
