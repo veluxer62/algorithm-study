a7a257086642e37c9209f18bd5d78605453e62ac
kei <veluxer62@gmail.com>
2024-11-13 22:27:07 +0900

    feat: 75. 입국심사

---
+++ src/test/java/com/example/algorithmstudy/ImmigrationTest.java
@@ -0,0 +1,51 @@
+package com.example.algorithmstudy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ImmigrationTest {
+    private final Solution sut = new Solution();
+
+    /*
+     * 각 심사관이 한 명을 심사하는 데 걸리는 시간이 담긴 배열 times가 주어질 때, 모든 사람이 심사를 받는 데 걸리는 시간의 최솟값을 출력하라.
+     */
+
+    @Test
+    public void test_migrate() {
+        var n = 6;
+        var times = new int[] {7, 10};
+        var actual = sut.migrate(n, times);
+        assertEquals(28, actual);
+    }
+
+    private static class Solution {
+        public long migrate(int n, int[] times) {
+            var answer = 0L;
+
+            var left = 1L;
+            var right = (long) Arrays.stream(times).max().orElse(0) * n;
+            var mid = right;
+
+            while (left <= right) {
+                var calcN = 0L;
+                for (var time : times) {
+                    calcN += mid / time;
+                }
+
+                if (calcN >= n) {
+                    answer = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+                mid = left + (right - left) / 2;
+            }
+
+            return answer;
+        }
+    }
+}
