e08ded63a2808cb2b264cdaf92a401bed58e7057
kei <veluxer62@gmail.com>
2024-11-15 23:44:08 +0900

    feat: 77. 해밍 거리

---
+++ src/test/kotlin/com/example/algorithmstudy/KotlinHammingDistanceTest.kt
@@ -0,0 +1,25 @@
+package com.example.algorithmstudy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class KotlinHammingDistanceTest {
+    /*
+     * 두 정수를 입력받아 몇 비트가 다른지 계산하라.
+     */
+
+    @Test
+    fun test_hammingDistance() {
+        val x = 1
+        val y = 6
+        val actual: Int = hammingDistance(x, y)
+        assertEquals(3, actual)
+    }
+
+    private fun hammingDistance(
+        x: Int,
+        y: Int,
+    ): Int {
+        return (x xor y).countOneBits()
+    }
+}
