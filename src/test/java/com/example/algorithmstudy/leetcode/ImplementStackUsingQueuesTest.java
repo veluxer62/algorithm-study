f33bae50f74dba3fb7f936941559f81e618b5a18
kei <veluxer62@gmail.com>
2025-01-10 14:20:01 +0900

    feat: 225. Implement Stack using Queues

---
+++ src/test/java/com/example/algorithmstudy/leetcode/ImplementStackUsingQueuesTest.java
@@ -0,0 +1,96 @@
+package com.example.algorithmstudy.leetcode;
+
+import java.util.LinkedList;
+
+public class ImplementStackUsingQueuesTest {
+    /*
+
+    Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+    Implement the MyStack class:
+
+    void push(int x) Pushes element x to the top of the stack.
+    int pop() Removes the element on the top of the stack and returns it.
+    int top() Returns the element on the top of the stack.
+    boolean empty() Returns true if the stack is empty, false otherwise.
+    Notes:
+
+    You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+    Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+
+
+    Example 1:
+
+    Input
+    ["MyStack", "push", "push", "top", "pop", "empty"]
+    [[], [1], [2], [], [], []]
+    Output
+    [null, null, null, 2, 2, false]
+
+    Explanation
+    MyStack myStack = new MyStack();
+    myStack.push(1);
+    myStack.push(2);
+    myStack.top(); // return 2
+    myStack.pop(); // return 2
+    myStack.empty(); // return False
+
+
+    Constraints:
+
+    1 <= x <= 9
+    At most 100 calls will be made to push, pop, top, and empty.
+    All the calls to pop and top are valid.
+
+
+    Follow-up: Can you implement the stack using only one queue?
+
+     */
+
+    private static class MyStack {
+        LinkedList<Integer> queue1;
+        LinkedList<Integer> queue2;
+
+        public MyStack() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            queue1.add(x);
+        }
+
+        public int pop() {
+            while (queue1.size() > 1) {
+                queue2.add(queue1.remove());
+            }
+
+            var top = queue1.remove();
+
+            var temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+
+            return top;
+        }
+
+        public int top() {
+            while (queue1.size() > 1) {
+                queue2.add(queue1.remove());
+            }
+
+            var top = queue1.peek();
+            queue2.add(queue1.remove());
+
+            var temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+
+            return top;
+        }
+
+        public boolean empty() {
+            return queue1.isEmpty();
+        }
+    }
+}
