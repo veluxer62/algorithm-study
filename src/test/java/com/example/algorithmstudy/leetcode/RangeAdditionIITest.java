ed698e95c3ff9352f73a9541d031b1c78b91f544
kei <veluxer62@gmail.com>
2025-01-30 21:03:46 +0900

    feat: 598. Range Addition II

---
+++ src/test/java/com/example/algorithmstudy/leetcode/RangeAdditionIITest.java
@@ -0,0 +1,75 @@
+package com.example.algorithmstudy.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RangeAdditionIITest {
+    private final Solution sut = new Solution();
+
+    /*
+
+    You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+
+    Count and return the number of maximum integers in the matrix after performing all the operations.
+
+
+
+    Example 1:
+
+
+    Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+    Output: 4
+    Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
+    Example 2:
+
+    Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+    Output: 4
+    Example 3:
+
+    Input: m = 3, n = 3, ops = []
+    Output: 9
+
+
+    Constraints:
+
+    1 <= m, n <= 4 * 104
+    0 <= ops.length <= 104
+    ops[i].length == 2
+    1 <= ai <= m
+    1 <= bi <= n
+
+     */
+
+    @Test
+    public void test_maxCount() {
+        var m = 3;
+        var n = 3;
+        var ops = new int[][]{{2, 2}, {3, 3}};
+        var actual = sut.maxCount(m, n, ops);
+        assertEquals(4, actual);
+
+        m = 3;
+        n = 3;
+        ops = new int[][]{{2, 2}, {3, 3}, {3, 3}, {3, 3}, {2, 2}, {3, 3}, {3, 3}, {3, 3}, {2, 2}, {3, 3}, {3, 3}, {3, 3}};
+        actual = sut.maxCount(m, n, ops);
+        assertEquals(4, actual);
+
+        m = 3;
+        n = 3;
+        ops = new int[][]{};
+        actual = sut.maxCount(m, n, ops);
+        assertEquals(9, actual);
+    }
+
+    private static class Solution {
+        public int maxCount(int m, int n, int[][] ops) {
+            for (int[] op : ops) {
+                m = Math.min(m, op[0]);
+                n = Math.min(n, op[1]);
+            }
+
+            return m * n;
+        }
+    }
+}
